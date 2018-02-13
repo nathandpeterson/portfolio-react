d65f494dbe378fa7f45842ed24513d2501b23ddf
Nathan Daniel Peterson <natperson@gmail.com>
2018-02-13 10:43:39 -0800

    first commit, new portfolio project

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './nav.css'
+
+const Nav = () => (
+  <nav>
+    <div className="nav-header">
+      <h3>about me</h3>
+      <h3>projects</h3>
+      <h3>blog</h3>
+      <h3>skills</h3>
+    </div>
+  </nav>
+)
+
+export default Nav
