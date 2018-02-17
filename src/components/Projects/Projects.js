68cac472a53dc3ca71955f5a5d47a05cf1c963ba
Nathan Daniel Peterson <natperson@gmail.com>
2018-02-17 13:30:07 -0800

    add images to projects and improve nav

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Projects.css'
-import projects from './project_data.jsons'
+import projects from './project_data.json'
 
-const Project = () => (
-  <div className="project-image">
-    <img src="" />
+const Project = ({ project }) => {
+  const { title, img, description } = project
+  return <div className="project-image">
+    <h5>{title}</h5>
+    <img src={img} alt={description}/>
   </div>
-)
+}
 
 const Projects = () => (
   <div className="projects-container">
@@ -14,9 +16,9 @@ const Projects = () => (
       <h1>Projects</h1>
     </div>
     <div className="project-browser">
-      <Project />
-      <Project />
-      <Project />
+      {projects.map((project, i) => {
+        return <Project key={i} project={ project } />
+      })}
     </div>
   </div>
 )
