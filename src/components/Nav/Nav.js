c148aa8326ad1bc30d392ecd1a6b18c8c5d45e14
Nathan Daniel Peterson <natperson@gmail.com>
2018-02-27 09:32:34 -0800

    improve responsiveness and add burger menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { scaleDown as Menu } from 'react-burger-menu'
 import './nav.css'
 
 const Nav = () => (
@@ -6,9 +7,13 @@ const Nav = () => (
     <div className="nav-header">
       <a href="#bio"><h3>about me</h3></a>
       <a href="#projects"><h3>projects</h3></a>
-      
       <a href="#competencies"><h3>skills</h3></a>
     </div>
+    <Menu width={'30%'}>
+      <a href="#bio"><p>about me</p></a>
+      <a href="#projects"><p>projects</p></a>
+      <a href="#competencies"><p>skills</p></a>
+    </Menu>
   </nav>
 )
 
